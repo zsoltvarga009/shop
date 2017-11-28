7878d9de7866180acee5bfeef1188844f11b2d2e
zsoltvarga009 <zsoltvarga@outlook.com>
2017-11-28 12:21:31 +0100

    Add files via upload

---
+++ Component.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel) {
+	"use strict";
+	return UIComponent.extend("sap.ui.demo.toolpageapp.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		},
+
+		getContentDensityClass: function () {
+			if (!this._sContentDensityClass) {
+				if (!sap.ui.Device.support.touch){
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		}
+	});
+});
